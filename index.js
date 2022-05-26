8a7cebc1e1e800fc8a9c04fa9a35830a30e9f971
doutenn <95846055+doutenn@users.noreply.github.com>
2022-05-26 17:16:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const main = () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+
+    const tateElement = document.getElementById('tate');
+	  console.log(tateElement.value);
+
+    const yokoElement = document.getElementById('yoko');
+	  console.log(yokoElement.value);
+
     //塗りつぶす色の指定
     ctx.fillStyle = "rgb(0, 155, 0)";
-   
-    let y = 400;
+
+    let x = 150;
+    let y1 = 0;
+    let y2 = 100;
   
     // スタートボタンの変数定義
     var startButton;
@@ -22,11 +31,11 @@ const main = () => {
     
         //スタートボタンが押された時の処理
         startButton.onclick = function() {
-          draw();
+          start();
         };
         //ストップボタンが押された時の処理
         stopButton.onclick = function() {
-            stop();
+          stop();
         };
         //リセットボタンが押された時の処理  
         resetButton.onclick = function() {
@@ -35,50 +44,114 @@ const main = () => {
       };
    
     function draw(){
-      if(y >= 100){
+      if(y1 >= 100){
        // canvas 内をまっさらにする
-       ctx.clearRect(0, 0, 800, 480);
+       ctx.clearRect(0, 0, 1000, 2000);
        //描画開始
        ctx.beginPath();
        //円
-       ctx.arc(150, y, 7, 0, Math.PI*2, false)
+       ctx.arc(150, y1, 7, 0, Math.PI*2, false)
        ctx.fill();
    
        //三角形
-       ctx.moveTo(150, y);
-       ctx.lineTo(150, 400);
-       ctx.lineTo(650, 400);
+       ctx.moveTo(150, y1);
+       ctx.lineTo(150, tateElement.value*100);
+       ctx.lineTo(yokoElement.value*100, tateElement.value*100);
        ctx.closePath();
        ctx.fill();
        
-       // y を 1 増加
-       y -= 1;
+       // y1 を 1 増加
+       y1 -= 1;
        // 10 ミリ秒(0.1 秒)後に draw を再度呼び出し
-       setTimeout(draw, 10, y - 1);
+       setTimeout(draw, 10, y1 - 1);
 
-  
        //四角形
        ctx.moveTo(150, 100);
-       ctx.lineTo(150, 400);
-       ctx.lineTo(650, 400);
-       ctx.lineTo(650, 100);
+       ctx.lineTo(150, tateElement.value*100);
+       ctx.lineTo(yokoElement.value*100, tateElement.value*100);
+       ctx.lineTo(yokoElement.value*100, 100);
    
        // 開始地点に戻って線を閉じる
        ctx.closePath();
        // 塗りつぶしを実行
        ctx.stroke();
-      }
+
+      }else if(y1 <= 100 && x <= yokoElement.value*100){
+        // canvas 内をまっさらにする
+        ctx.clearRect(0, 0, 1000, 2000);
+        //描画開始
+        ctx.beginPath();
+        //円
+        ctx.arc(x, 100, 7, 0, Math.PI*2, false)
+        ctx.fill();
+    
+        //三角形
+        ctx.moveTo(x, 100);
+        ctx.lineTo(150, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, tateElement.value*100);
+        ctx.closePath();
+        ctx.fill();
+   
+        // x を 1 増加
+        x += 1;
+        // 10 ミリ秒(0.01 秒)後に draw を再度呼び出し
+        setTimeout(draw, 10, x + 1);
+   
+        //四角形
+        ctx.moveTo(150, 100);
+        ctx.lineTo(150, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, 100);
+    
+        // 開始地点に戻って線を閉じる
+        ctx.closePath();
+        // 塗りつぶしを実行
+        ctx.stroke();
+      }else if(y1 <= 100 && x >= yokoElement.value*100 && y2 <= tateElement.value*100){
+        // canvas 内をまっさらにする
+        ctx.clearRect(0, 0, 1000, 2000);
+        //描画開始
+        ctx.beginPath();
+        //円
+        ctx.arc(yokoElement.value*100, y2, 7, 0, Math.PI*2, false)
+        ctx.fill();
+    
+        //三角形
+        ctx.moveTo(yokoElement.value*100, y2);
+        ctx.lineTo(150, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, tateElement.value*100);
+        ctx.closePath();
+        ctx.fill();
+        
+        // y2 を 1 増加
+        y2 += 1;
+        // 10 ミリ秒(0.1 秒)後に draw を再度呼び出し
+        setTimeout(draw, 10, y2 + 1);
+ 
+   
+        //四角形
+        ctx.moveTo(150, 100);
+        ctx.lineTo(150, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, tateElement.value*100);
+        ctx.lineTo(yokoElement.value*100, 100);
+    
+        // 開始地点に戻って線を閉じる
+        ctx.closePath();
+        // 塗りつぶしを実行
+        ctx.stroke();
+       }
     };
 
-    function stop() {
-        clearInterval(y);
-        y = null;
+    function start() {
+        y1 = tateElement.value*100;
+        draw(); 
     }
 
     function reset() {
-        y = 400;
-        draw();
-        
+        x = 150;
+        y1 = tateElement.value*100;
+        y2 = 100;
+        draw(); 
     }
   };
 
