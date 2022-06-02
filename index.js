ec16a54bd778633075c4c0079de58f0427daa07b
doutenn <95846055+doutenn@users.noreply.github.com>
2022-06-02 16:41:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const main = () => {
+
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
+    
+    //縦の長さのスライダー
+    function inputChange1(event){
+      msg1.innerText = '縦の長さは ' + tateElement.value + ' cmです';
+    }
     const tateElement = document.getElementById('tate');
+    tateElement.addEventListener('input', inputChange1);
 	  console.log(tateElement.value);
+    let msg1 = document.getElementById('msg1');
 
+    //横の長さのスライダー
+    function inputChange2(event){
+      msg2.innerText = '横の長さは ' +  yokoElement.value + ' cmです';
+    }
     const yokoElement = document.getElementById('yoko');
+    yokoElement.addEventListener('input', inputChange2);
 	  console.log(yokoElement.value);
+    let msg2 = document.getElementById('msg2');
+
+    //速さのスライダー
+    function inputChange3(event){
+      msg3.innerText = '速さは ' +  hayasaElement.value + ' cm/sです';
+    }
+    const hayasaElement = document.getElementById('hayasa');
+    hayasaElement.addEventListener('input', inputChange3);
+	  console.log(hayasaElement.value);
+    let msg3 = document.getElementById('msg3');
 
     //塗りつぶす色の指定
     ctx.fillStyle = "rgb(0, 155, 0)";
@@ -46,9 +68,10 @@ const main = () => {
     function draw(){
       if(y1 >= 100){
        // canvas 内をまっさらにする
-       ctx.clearRect(0, 0, 1000, 2000);
+       ctx.clearRect(0, 0, 2000, 3000);
        //描画開始
        ctx.beginPath();
+      
        //円
        ctx.arc(150, y1, 7, 0, Math.PI*2, false)
        ctx.fill();
@@ -61,9 +84,9 @@ const main = () => {
        ctx.fill();
        
        // y1 を 1 増加
-       y1 -= 1;
+       y1 -= hayasaElement.value;
        // 10 ミリ秒(0.1 秒)後に draw を再度呼び出し
-       setTimeout(draw, 10, y1 - 1);
+       setTimeout(draw, 10);
 
        //四角形
        ctx.moveTo(150, 100);
@@ -75,10 +98,16 @@ const main = () => {
        ctx.closePath();
        // 塗りつぶしを実行
        ctx.stroke();
+       //点の名前
+       ctx.font = "32px serif";
+       ctx.fillText("A",120,tateElement.value*100+10)
+       ctx.fillText("B",120,110)
+       ctx.fillText("C",yokoElement.value*100+10,110)
+       ctx.fillText("D",yokoElement.value*100+10,tateElement.value*100+10)
 
       }else if(y1 <= 100 && x <= yokoElement.value*100){
         // canvas 内をまっさらにする
-        ctx.clearRect(0, 0, 1000, 2000);
+        ctx.clearRect(0, 0, 2000, 3000);
         //描画開始
         ctx.beginPath();
         //円
@@ -93,9 +122,9 @@ const main = () => {
         ctx.fill();
    
         // x を 1 増加
-        x += 1;
+        x += 2;
         // 10 ミリ秒(0.01 秒)後に draw を再度呼び出し
-        setTimeout(draw, 10, x + 1);
+        setTimeout(draw, 10);
    
         //四角形
         ctx.moveTo(150, 100);
@@ -107,9 +136,15 @@ const main = () => {
         ctx.closePath();
         // 塗りつぶしを実行
         ctx.stroke();
+        //点の名前
+        ctx.font = "32px serif";
+        ctx.fillText("A",120,tateElement.value*100+10)
+        ctx.fillText("B",120,110)
+        ctx.fillText("C",yokoElement.value*100+10,110)
+        ctx.fillText("D",yokoElement.value*100+10,tateElement.value*100+10)
       }else if(y1 <= 100 && x >= yokoElement.value*100 && y2 <= tateElement.value*100){
         // canvas 内をまっさらにする
-        ctx.clearRect(0, 0, 1000, 2000);
+        ctx.clearRect(0, 0, 2000, 3000);
         //描画開始
         ctx.beginPath();
         //円
@@ -124,9 +159,9 @@ const main = () => {
         ctx.fill();
         
         // y2 を 1 増加
-        y2 += 1;
+        y2 += 2;
         // 10 ミリ秒(0.1 秒)後に draw を再度呼び出し
-        setTimeout(draw, 10, y2 + 1);
+        setTimeout(draw, 10, y2 + 2);
  
    
         //四角形
@@ -139,6 +174,12 @@ const main = () => {
         ctx.closePath();
         // 塗りつぶしを実行
         ctx.stroke();
+        //点の名前
+        ctx.font = "32px serif";
+        ctx.fillText("A",120,tateElement.value*100+10)
+        ctx.fillText("B",120,110)
+        ctx.fillText("C",yokoElement.value*100+10,110)
+        ctx.fillText("D",yokoElement.value*100+10,tateElement.value*100+10)
        }
     };
 
